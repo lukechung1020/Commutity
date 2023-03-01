0b4f3db799ce0c7100bc41f39f2f1f1b82aa303d
EllaIca <myuu.lome@gmail.com>
2023-02-28 22:09:04 -0800

    fixing all the flow of app

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -2,7 +2,7 @@
 // This function loads the parts of your skeleton 
 // (navbar, footer, and other things) into html doc. 
 //---------------------------------------------------
-function loadSkeleton(){
+function loadSkeleton() {
     console.log($('#navbarPlaceholder').load('./app-elements/nav.html'));
     console.log($('#footerPlaceholder').load('./app-elements/footer.html'));
 }
