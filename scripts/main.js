3b430d0a020a96ab103e22822ef52d1041d64137
EllaIca <myuu.lome@gmail.com>
2023-03-29 17:40:19 -0700

    added delete function

--- scripts/main.js
+++ scripts/main.js
@@ -1,54 +1,57 @@
-// function readText(text) {
-//     db.collection("posts").doc(text)
-//         .onSnapshot(eachText => {
-//             console.log("current document data: " + eachText.data());
-//             document.getElementById("postText-goes-here").innerHTML = eachText.data().postText;
-//         })
-// }
 
-// readText("jBR5cx50M34PihUqlYhp");
-
-// function readTitle(title) {
-//     db.collection("posts").doc(title)
-//         .onSnapshot(eachTitle => {
-//             console.log("current document data: " + eachTitle.data());
-//             document.getElementById("title-goes-here").innerHTML = eachTitle.data().postTitle;
-//         })
-// }
-
-// readTitle("jBR5cx50M34PihUqlYhp"); 
-//------------------------------------------------------------------------------
-// Input parameter is a string representing the collection we are reading from
-//------------------------------------------------------------------------------
 
 function displayPosts(collection) {
     let cardTemplate = document.getElementById("postsCardTemplate");
     let noImagecardTemplate = document.getElementById("n-postsCardTemplate");
 
-    db.collection(collection).get()   
+    db.collection(collection).get()
         .then(allPosts => {
-            allPosts.forEach(doc => { 
+            allPosts.forEach(doc => {
                 var title = doc.data().postTitle;
                 var postText = doc.data().postText;
-                var timeStamp = doc.data().timestamp; 
+                var timeStamp = doc.data().timestamp;
                 var userID = doc.data().userID;
-                var image = doc.data().image; 
+                var image = doc.data().image;
                 let newcard = cardTemplate.content.cloneNode(true);
-                
+
                 newcard.querySelector('.card-title').innerHTML = title;
                 newcard.querySelector('.card-text').innerHTML = postText;
                 console.log()
-                if (!(image === "" )) {
+                if (!(image === "")) {
                     newcard.querySelector('.card-image').src = image;
                 } else {
                     newcard.querySelector('.card-image').src = "./images/icon.jpg";
                 }
-                
+
                 document.getElementById(collection + "-go-here").appendChild(newcard);
-                
+
+                let deleteButton = document.querySelector('#delete-button');
+                $(deleteButton).click(() => {
+                    if (
+                        window.confirm("Are you sure you want to delete this post?")
+                    ) {
+                        doc.ref.delete();
+                        $(newcard).remove();
+                    }
+                })
+
             })
         })
+
+
 }
 
-displayPosts("posts") 
+displayPosts("posts");
+
+// const deletePostForm = document.querySelector('.delete')
+// deletePostForm.addEventListener('submit', (e) => {
+//     e.preventDefault()
+
+//     const docRef = doc(db, 'posts', deletePostForm.id.value)
+
+//     deleteDoc(docRef)
+//         .then(() => {
+//             deletePostForm.reset()
+//         })
+// })
 
