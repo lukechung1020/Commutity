79f60cacab8dd2b86730f5eb70a408d5f7c7f532
EllaIca <114512576+EllaIca@users.noreply.github.com>
2023-04-04 17:58:52 -0700

    updated . should function now

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,27 +1,31 @@
 var currentUser;
 
+
 function populateUserInfo() {
     firebase.auth().onAuthStateChanged(user => {
         // Check if user is signed in:
         if (user) {
-
             //go to the correct user document by referencing to the user uid
             currentUser = db.collection("users").doc(user.uid);
             //get the document for current user.
             currentUser.get()
                 .then(userDoc => {
                     //get the data fields of the user
-                    var userName = userDoc.data().posts;
+                    var userName = userDoc.data().name;
+                    var userEmail = userDoc.data().email;
+                    var userPost = userDoc.data().posts;
                     var userCommuteType = userDoc.data().commuteType;
                     var userCity = userDoc.data().city;
 
-                    //if the data fields are not empty, then write them in to the form.
+                    var displayUserName = document.getElementById("user-name");
+                    displayUserName.innerText = userName;
+                    // if the data fields are not empty, then write them in to the form.
                     if (userName != null) {
                         document.getElementById("nameInput").value = userName;
                     }
-                    if (userCommuteType != null) {
-                        document.getElementById("commuteSelect").value = userCommuteType;
-                    }
+                    // if (userCommuteType != null) {
+                    //     document.getElementById("commuteSelect").value = userCommuteType;
+                    // }
                 })
         } else {
             // No user is signed in.
@@ -43,19 +47,15 @@ function saveUserInfo() {
 
     //a) get user entered values
     userName = document.getElementById('nameInput').value;       //get the value of the field with id="nameInput"
-    userCommuteType = document.getElementById('commuteSelect').value;     //get the value of the field with id="schoolInput"
-    userCity = document.getElementById('citySelect').value;       //get the value of the field with id="cityInput"
     //b) update user's document in Firestore
     currentUser.update({
-        name: userName,
-        commuteType: userCommuteType,
-        city: userCity
+        name: userName
     })
         .then(() => {
             console.log("Document successfully updated!");
+            window.location.reload();
         })
     //c) disable edit 
     document.getElementById('personalInfoFields').visible = false;
-
 }
 
